7192c3bbedaae936791e4ab71d2f17f079b64ed6
Let0oro <oniiichan354@gmail.com>
2024-09-07 17:21:58 +0200

    fix: unused openAi library

--- src/utils/evaluateOriginality.cjs
+++ src/utils/evaluateOriginality.cjs
@@ -1,25 +1,25 @@
 require("dotenv").config();
-const OpenAI = require("openai");
-const openai = new OpenAI({ apiKey: process.env.API_KEY_OPENAI });
+// const OpenAI = require("openai");
+// const openai = new OpenAI({ apiKey: process.env.API_KEY_OPENAI });
 
 const evaluateOriginality = async (poem) => {
   
     return Math.random();
   
 //!  This was the legal way
-  await openai.chat.completions.create({
-    model: "gpt-3.5-mini",
-    messages: [
-      {
-        role: "system",
-        content: "You are a very helpful linguistics teacher.",
-      },
-      {
-        role: "user",
-        content: `Evaluate between 0 and 1 the originality and rhetorical devices in this poem: ${poem}`,
-      },
-    ],
-  });
+  // await openai.chat.completions.create({
+  //   model: "gpt-3.5-mini",
+  //   messages: [
+  //     {
+  //       role: "system",
+  //       content: "You are a very helpful linguistics teacher.",
+  //     },
+  //     {
+  //       role: "user",
+  //       content: `Evaluate between 0 and 1 the originality and rhetorical devices in this poem: ${poem}`,
+  //     },
+  //   ],
+  // });
 };
 
 module.exports = evaluateOriginality;
